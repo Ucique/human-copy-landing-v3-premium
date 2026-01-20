7e1bcebccc7a3aebc7a933e6c3f09e151aaf83af
Ucique <uciqueme@gmail.com>
2026-01-19 23:05:43 -0800

    Add legal pages and refine landing form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingpageV3Preview from "./LandingpageV3Preview.jsx";
+import Datenschutz from "./pages/Datenschutz.jsx";
+import Impressum from "./pages/Impressum.jsx";
 
 export default function App() {
-  return <LandingpageV3Preview />;
+  return (
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <Routes>
+        <Route path="/" element={<LandingpageV3Preview />} />
+        <Route path="/impressum" element={<Impressum />} />
+        <Route path="/datenschutz" element={<Datenschutz />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
