0cf88f43c59bbbb93c5e42c26be36c9779403b78
Ucique <uciqueme@gmail.com>
2026-01-19 21:05:31 -0800

    Fix Vite React SWC plugin dependency

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "vite-plugin-react-swc";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   base: "/human-copy-landing-v3-premium/",
